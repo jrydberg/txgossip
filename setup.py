eaf2cc314b721dba2de42169ab0d635fcf46c0e8
Johan Rydberg <johan.rydberg@gmail.com>
2011-08-29 14:04:50 +0200

    Packaging stuff.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='txgossip',
+      version='0.0',
+      description='simple cluster communication protocol',
+      author='Johan Rydberg',
+      author_email='johan.rydberg@gmail.com',
+      url='http://github.com/jrydberg/txgossip',
+      packages=find_packages()
+)
